56c244ccf1a373a31d42a69abd9766359851387e
AnnaMGh <33024582+AnnaMGh@users.noreply.github.com>
2017-12-18 09:39:49 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,9 +2,6 @@ var game = new Phaser.Game(1350, 480, Phaser.CANVAS);
 
 game.state.add('Menu', Technotip.Menu);
 game.state.add('LevelOne', Technotip.LevelOne);
+game.state.add('LevelTwo', Technotip.LevelTwo);
 
 game.state.start('Menu');
-
-//game.state.start('LevelOne');
-
-//window.alert('After load');
